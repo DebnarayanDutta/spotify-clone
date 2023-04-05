6562e951cbafa9b6679ceee0069d57fc458e7385
Debnarayan <debnarayanduttaz663@gmail.com>
2023-04-06 00:39:33 +0530

    first commit

---
+++ src/Components/Songs.js
@@ -0,0 +1,93 @@
+const Songs = [
+    {
+      id: 1,
+      favourite: false,
+      songName: "On & On",
+      artist: "Cartoon, Daniel Levi",
+      song: "https://ncs.io/track/download/442dfd89-c291-41b4-b93a-18c0ff73c1dc",
+      imgSrc:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/152/325x325/on-on-feat-daniel-levi-1586947008-oeI24in2Ga.jpg",
+    },
+    {
+      id: 2,
+      favourite: false,
+      songName: "Linked",
+      artist: "Jim Yosef, Anna Yvette",
+      song: "https://ncs.io/track/download/f27e0c33-90d6-41d2-8e04-67f60fd68134",
+      imgSrc:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/434/325x325/linked-1586953317-0BranYHgw8.jpg",
+    },
+    {
+      id: 3,
+      favourite: false,
+      songName: "Heroes Tonight",
+      artist: "Janji, Johnning",
+      song: "https://ncs.io/track/download/2ede9de2-8386-4210-b2a0-b36d731c8a4e",
+      imgSrc:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/143/325x325/heroes-tonight-feat-johnning-1586946923-LsvZKuKHRf.jpg",
+    },
+    {
+      id: 4,
+      favourite: false,
+      songName: "Mortals",
+      artist: "Warriyo, Laura Brehm",
+      song: "https://ncs.io/track/download/784a2ccc-5ace-48d1-8af1-9da55c383960",
+      imgSrc:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/287/325x325/mortals-feat-laura-brehm-1586948734-yFnA6l5Geq.jpg",
+    },
+    {
+      id: 5,
+      favourite: false,
+      songName: "Make Me Move",
+      artist: "Culture Code, KARRA",
+      song: "https://ncs.io/track/download/00475073-2e06-4c49-bbd8-e1c1797d5697",
+      imgSrc:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/286/325x325/make-me-move-feat-karra-1586948717-dTsA9So1cu.jpg",
+    },
+    {
+      id: 6,
+      songName: "Fearless",
+      artist: "TULE",
+      song: "https://ncs.io/track/download/65b8d9a0-5099-4f5c-b0b2-c73c608ce220",
+      imgSrc:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/396/325x325/fearless-1586951958-jR0GTf1Swx.jpg",
+    },
+    {
+      id: 7,
+      favourite: false,
+      songName: "Why we lose",
+      artist: "Cartoon, Coleman Trapp",
+      song: "https://ncs.io/track/download/84a06f66-d649-4224-b3a3-d35857fcf752",
+      imgSrc:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/144/325x325/why-we-lose-feat-coleman-trapp-1586946931-cMa5r9VQ4S.jpg",
+    },
+    {
+      id: 8,
+      favourite: false,
+      songName: "Perfect 10",
+      artist: "Unknown Brain, Heather Sommer",
+      song: "https://ncs.io/track/download/1feef04e-13dd-4d41-b68c-b28442f3d97c",
+      imgSrc:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/510/325x325/perfect-10-feat-heather-sommer-1586955170-wRtg5YiQiu.jpg",
+    },
+    {
+      id: 9,
+      favourite: false,
+      songName: "Feel Good",
+      artist: "Syn Cole",
+      song: "https://ncs.io/track/download/be608f27-74a5-4785-9a7d-192fdaf0dd4e",
+      imgSrc:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/205/325x325/feel-good-1586947464-m1y8kbZ5x7.jpg",
+    },
+    {
+      id: 10,
+      favourite: false,
+      songName: "Royalty",
+      artist: "Maestro Chives, Egzod, Neoni",
+      song: "https://ncs.io/track/download/2b08f7d9-4f5f-47bd-a608-07dec1f7eaa9",
+      imgSrc:
+        "https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/936/325x325/royalty-1619082030-xBgqGZWLw9.jpg",
+    },
+  ];
+  
+  export { Songs };
